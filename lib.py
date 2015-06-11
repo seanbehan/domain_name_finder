f466d1054f67379732707ae8280ec9fca1cd1fa5
Sean Behan <inbox@seanbehan.com>
2015-06-11 19:10:21 -0400

    template for resellers

--- lib.py
+++ lib.py
@@ -4,6 +4,7 @@ from lxml import html
 from itertools import permutations
 import marshal
 from random import shuffle
+from jinja2 import Template
 
 def stopwords():
     return marshal.loads(open("stopwords.marshal").read())
@@ -29,9 +30,15 @@ def query_domain(domains):
             return result
 
 def domains(url):
+    t = Template(open("domain.html").read())
+
     for g in groups(words_from_url(url)):
         x = query_domain(' '.join(['%s.com' % (w) for w in [''.join(w) for w in list(permutations(g))]]))
         if x:
-            yield x + '<br>'
+            x = x.replace("** server can't find", "")
+            x = x.replace(": NXDOMAIN", "")
+            x = x.strip()
+
+            yield t.render(domain=x)
         else:
             yield ''
